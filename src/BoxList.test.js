de231e4c9a62444ca77f6f55238d251e323a2085
Dare3 <enitan_segun@yahoo.co.uk>
2024-09-21 22:50:17 -0400

    test

--- src/BoxList.test.js
+++ src/BoxList.test.js
@@ -1,8 +1,25 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import BoxList from './BoxList';
 
 test('renders BoxList component', () => {
   const { asFragment } = render(<BoxList />);
   expect(asFragment()).toMatchSnapshot();
+});
+
+
+test('removes a box when the remove button is clicked', () => {
+  const { getByText, queryByText } = render(<BoxList />);
+  
+  // Add a box first to remove it
+  fireEvent.click(getByText(/Add Box/i)); // Assuming initial values are set in NewBoxForm
+
+  // Now there should be a box
+  expect(getByText(/X/i)).toBeInTheDocument();
+
+  // Click the remove button
+  fireEvent.click(getByText(/X/i));
+
+  // Now the box should be gone
+  expect(queryByText(/X/i)).not.toBeInTheDocument();
 });
